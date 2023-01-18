26eee6e45abd8e120ad33b64856271e8deba90f4
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-18 20:38:03 +0530

    Update __init__.py

--- SPLFW/__init__.py
+++ SPLFW/__init__.py
@@ -1,1 +1,12 @@
+from hlpl_english_words import english_words
 
+case=['adjective','adverb','noun-singular','noun-plural','verb','article']
+
+WORDS = []
+
+def load_words():
+    global WORDS 
+    for mad in case:
+        for red in english_words.get(mad):
+            if len(red) >= 7:
+                WORDS.append(red)
