e4fdab32c9a8a0ce139b98f7d5d6e863dc8a867f
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-18 20:30:16 +0530

    Update changetime.py

--- SPLFW/changetime.py
+++ SPLFW/changetime.py
@@ -1,2 +1,19 @@
 from pyrogram import Client, filters 
-from SPLFW.Database.
+from SPLFW.Database.limit import *
+
+@Client.on_message(filters.command("change") & filters.group)
+async def change(_, m):
+    if len(m.command) != 2:
+        return await m.reply("`/change (100-1000)`")
+    try:
+        value = int(m.text.split()[1])
+    except:
+        return await m.reply("`/change (100-1000)`")
+    if (value < 100) or (value > 1000):
+        return await m.reply("`/change (100-1000)`")
+    x = await _.get_chat_member(m.chat.id, m.from_user.id)
+    if not x.status.name in ["OWNER", "ADMINISTRATOR"]:
+        return await m.reply("`Only group staff can change !`")
+    y = await get_chat_limit(m.chat.id)
+    await update_chat_limit(m.chat.id, value)
+    await m.reply(f"`value has been changed from {y} to {value}.`")
