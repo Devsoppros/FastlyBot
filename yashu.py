6784ba874bee07c8761160f63a31278f1c23c799
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-18 21:35:36 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,8 +1,11 @@
 from pyrogram import Client, filters, idle
 from config import *
+from SPLFW import load_bg, load_words
 
 yashu = Client(":SPLFW:", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="SPLFW"))
 
+load_words()
+load_bg()
 yashu.start()
 print("Bot Started !")
 idle()
