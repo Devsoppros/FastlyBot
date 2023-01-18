f713e60ee3bc289f76348368b46dd3b039de8e8b
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-18 20:39:51 +0530

    Update paste.py

--- SPLFW/paste.py
+++ SPLFW/paste.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
+from . import WORDS
+import random
 
 def create_image(path, text):
     im = Image.open(path)
     font = ImageFont.truetype("./Fonts/test2.ttf")
     draw = ImageDraw.Draw(im)
+    text = random.choice(WORDS)
