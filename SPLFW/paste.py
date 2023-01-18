c5486a9f887e7f1c4aa810a3e9b20beeca90caf5
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-18 20:58:46 +0530

    Update paste.py

--- SPLFW/paste.py
+++ SPLFW/paste.py
@@ -1,12 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
-from . import WORDS
-import random
 
 def create_image(path, text):
     im = Image.open(path)
     font = ImageFont.truetype("./Fonts/test2.ttf", 30)
     draw = ImageDraw.Draw(im)
-    text = random.choice(WORDS)
     w, h = draw.textsize(text, font)
     W, H = im.size
     coord = ((W-w)/2, (H-h)/2)
