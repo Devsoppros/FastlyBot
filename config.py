b5be5cb2e8085908e03632375d7529e8f0e6800d
Devsoppros <102241186+Devsoppros@users.noreply.github.com>
2023-01-18 21:34:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "8460373"))
+API_HASH = getenv("API_HASH", "83d8e423197251216303abfcbed9e820")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "5613329802:AAHu6FjxgiSMS_BdneH_xiNTNovTYiulM4I")
 
-MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Lightyagami10:light@cluster0.zo6bf4g.mongodb.net/?retryWrites=true&w=majority")
 
-BACKGROUND_URL = getenv("BACKGROUND_URL", "")
+BACKGROUND_URL = getenv("BACKGROUND_URL", "https://telegra.ph/file/2d1d2cb6d5ba6918be386.jpg")
